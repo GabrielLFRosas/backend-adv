e5581a54134b8c3ee338408ef98e024d1de82f4d
Gabriel Rosas <gabriel.rosas@techtools.vc>
2025-05-03 11:25:41 -0300

    CORS try fix

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['*'], // ou '*' (não recomendado em produção)
-    credentials: true, // se estiver usando cookies/autenticação
+    origin: 'https://seufrontend.com', // Substitua pelo domínio correto
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // se estiver usando sessões ou JWT com cookies
   });
+  
   await app.listen(3000);
 }
 bootstrap();
