f2dd417615e4e67eabf6d12069c0c06b2b1cd4a5
Gabriel Rosas <gabriel.rosas@techtools.vc>
2025-05-03 11:19:56 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['*'], // ou '*' (não recomendado em produção)
+    credentials: true, // se estiver usando cookies/autenticação
+  });
   await app.listen(3000);
 }
 bootstrap();
