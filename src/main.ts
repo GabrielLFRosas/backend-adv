31990682e72baa171da6cb16f73a97a0ff0b19da
Gabriel Rosas <gabriel.rosas@techtools.vc>
2025-05-13 11:14:53 -0300

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'], // Substitua pelo domínio correto
+    origin: ['http://localhost:3000', 'https://controle-alyne.vercel.app'], // Substitua pelo domínio correto
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // se estiver usando sessões ou JWT com cookies
